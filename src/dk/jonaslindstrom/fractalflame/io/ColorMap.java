4fa46fe7839351206eec33f8c60c5d95815b6a1a
Jonas Lindstrøm <jonas.l.jensen@alexandra.dk>
2018-01-15 23:22:17 +0100

    Refactoring and some doc

--- src/dk/jonaslindstrom/fractalflame/io/ColorMap.java
+++
@@ -1,8 +0,0 @@
-package dk.jonaslindstrom.fractalflame.io;
-
-import java.awt.Color;
-import java.util.function.Function;
-
-public interface ColorMap extends Function<int[][], Color[][]> {
-
-}
--- src/dk/jonaslindstrom/fractalflame/transformations/Transformation.java
+++
@@ -1,9 +0,0 @@
-package dk.jonaslindstrom.fractalflame.transformations;
-
-import java.util.function.UnaryOperator;
-
-import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
-
-public interface Transformation extends UnaryOperator<Vector2D> {
-
-}
